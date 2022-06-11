699a58c0e5c3f33507e6b6c9fcb9a563a856bd33
ausafaq <ausaf.ahmed@hotmail.com>
2022-06-11 14:17:40 -0700

    Implement countdown using multiplexing

---
+++ countdown/countdown.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+
+	abort := make(chan struct{})
+	go func() {
+		os.Stdin.Read(make([]byte, 1)) // read a single byte
+		abort <- struct{}{}
+	}()
+
+	fmt.Println("Commencing countdown. Press return to abort.")
+	ticker := time.NewTicker(1 * time.Second)
+	for countdown := 10; countdown > 0; countdown-- {
+		fmt.Println(countdown)
+		select {
+		case <-ticker.C:
+			// do nothing
+		case <-abort:
+			fmt.Println("Launch aborted!")
+			return
+		}
+	}
+	ticker.Stop()
+	launch()
+}
+
+func launch() {
+	fmt.Println("Lift off!")
+}
